fb2e0163599d52785cd7d59aad7e42c4b652cc64
rtdev-spec <rtzikadeveloper@gmail.com>
2025-02-09 01:35:38 -0300

    Segundo commit

--- index/index.js
+++ index/index.js
@@ -1,1 +1,37 @@
-console.log("Repository Online")
+// experimentando os comandos let e var
+let prata = "1000 PONTOS"
+let heroiP = "prata"
+console.log ("Se o héroi primeiro possui " + prata + " logo, ele é " + heroiP)
+
+let bronze = 1001
+let bronzeD = 2000
+let heroiB = "bronze"
+console.log ("Se o héroi segundo héroi possui entre " + bronze + " e " + bronzeD + " logo, ele será " + heroiB )
+
+let ferro = 2001
+let ferroD = 5000
+let heroiF = "Ferro"
+console.log ("Se o héroi terceiro possui entre " + ferro + " e " + ferroD + " logo, ele será " + heroiF)
+
+var ouro = 5001
+var ouroD = "7000 PONTOS,"
+var heroiO = " logo, ele é Ouro"
+console.log ("Se o quarto héroi possui entre " + ouro + " e " + ouroD + heroiO)
+
+var platina = " 7001 e 8000"
+let platinad = " logo, ele é platina"
+console.log("Se o quinto héroi possui entre" + platina + platinad)
+
+let ascendente = " possuir entre 8001 e 9000 pontos "
+let ascendenteD = "Logo, este héroi é o Ascendente!"
+console.log("Se" + ascendente + ascendenteD)
+
+var i = "Se possuir entre 9001 e 10.000 pontos, "
+var i2 = "Será o temido imortal"
+console.log (i + i2)
+
+
+// Poderia apenas ter adicionado console.log("mensagem") para diminuir o código.
+// mas o intuito era brincar um pouco.
+let s = "Este é o histórico de XP dos hérois."
+console.log(s)
---
+++ index/readme.md
@@ -0,0 +1,7 @@
+## Utilitários
+// let e var
+Criei um index.js, aberto;
+o intuito foi escrever códigos e diminuir conforme aumentasse os hérois.
+
+## Saída
+É um prazer participar destes projetos da Dio.me
